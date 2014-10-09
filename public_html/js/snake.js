ec1f21f542cbfb55c6957c21ff364a652e5e735d
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2014-10-09 14:02:22 -0700

    On video 10

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -5,7 +5,14 @@ var screenWidth;
 var screenHeight;
 
 function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getCanvas("2d");
     
+    screenWidth = window.screenWidth;
+    screenHeight = window.screenHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 
 function gameLoop() {
