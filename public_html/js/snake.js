6466be533ad50bdd554ae57e66aca9f24c7e32ca
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2014-10-30 18:13:24 -0700

    On video 55

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -17,6 +17,7 @@ var gameState;
 var gameOverMenu;
 var restartButton;
 var playHUD;
+var scoreboard;
 /* -----------------------------------------------------------------------------
  *  Executing Game Code
  * -----------------------------------------------------------------------------
@@ -47,12 +48,14 @@ function gameInitialize() {
     restartButton.addEventListener("click", gameRestart);
     
     playHUD = document.getElementById("playHUD");
+    scoreboard =document.getElementById("scoreboard");
 
     setState("PLAY");
 }
 
 function gameLoop() {
     gameDraw();
+    drawScoreboard();
     if (gameState == "PLAY") {
         snakeUpdate();
         snakeDraw();
@@ -236,4 +239,8 @@ function showMenu(state) {
 function centerMenuPosition(menu) {
     menu.style.top = (screenHeight / 2) - (menu.offsetHeight/2) + "px";
     menu.style.left = (screenWidth / 2) - (menu.offsetWidth/2) + "px";
+}
+
+function drawScoreboard() {
+    scoreboard.innerHTML = "Length: " + snakeLength;
 }
